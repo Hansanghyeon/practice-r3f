47577bf358d6dc6ad9073ca43773c5ac3ff33902
Hansanghyeon <dandan9509@gmail.com>
2020-11-02 19:34:59 +0900

    feat: 정적 빌드

--- docs/_next/static/VKPYw_ho8hxfDOX2GLwKU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Ffb7d5399.cedad88fb9e0b43984a5.js","static\u002Fchunks\u002Fpages\u002Findex-758684ad2c0d01d08db2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f4f567df00ce0843964e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://hansanghyeon.github.io/practice-r3f"
+      : "",
+};
